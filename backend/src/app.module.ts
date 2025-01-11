bc3bbf1a1315512623a195a9b6450d66215ca282
Hong LY <deprohong@gmail.com>
2025-01-11 23:07:20 +0700

    feat: implement user modules and bycrypt password with hash

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './lib/config';
 import { JwtModule } from './lib/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import dbConfig from './database/db.config';
+import { UserModule } from './api/user/user.module';
 
 @Module({
-  imports: [ConfigModule, JwtModule],
+  imports: [
+    TypeOrmModule.forRootAsync({ useFactory: dbConfig }),
+    ConfigModule,
+    JwtModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
